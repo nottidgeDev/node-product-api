72cca299d7f4007b78ea030e8065ab3afd6526d2
Nottidge Chibueze <nottidgechibueze042@gmail.com>
2026-01-12 12:50:04 +0100

    Modified project to run just my primenexus app

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.use(express.static(resolve('./public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, './PrimeNexus-app/index.html'));
+});
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running at http://localhost:3000');
+});
