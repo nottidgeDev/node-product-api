d1304b27f13a0132631524fc0ae8137229ba577d
Nottidge Chibueze <nottidgechibueze042@gmail.com>
2026-03-09 05:08:41 +0100

    Adjusted port access for both in production and when run locally

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ app.use(express.static(resolve('./public')));
 
 
 
-
-
-app.listen(3000, () => {
-  console.log('Server is running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
